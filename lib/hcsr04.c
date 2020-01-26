e3303b3d65cbc5c9a3d99a9355dc604f17b14136
黄艺华 <lernanto.wong@gmail.com>
2020-01-26 23:32:26 +0800

    HC-SR04 驱动添加异步读取

--- lib/hcsr04.c
+++ lib/hcsr04.c
@@ -12,13 +12,20 @@ int hcsr04_init(
     GPIO_TypeDef *trig_port,
     uint16_t trig_pin,
     GPIO_TypeDef *echo_port,
-    uint16_t echo_pin
+    uint16_t echo_pin,
+    uint32_t timeout
 )
 {
     hcsr04->trig_port = trig_port;
     hcsr04->trig_pin = trig_pin;
     hcsr04->echo_port = echo_port;
     hcsr04->echo_pin = echo_pin;
+    hcsr04->timeout = timeout;
+    hcsr04->state = HCSR04_RESET;
+    hcsr04->echo_begin_ms = 0;
+    hcsr04->echo_begin_tick = 0;
+    hcsr04->echo_end_ms = 0;
+    hcsr04->echo_end_tick = 0;
 
     HAL_GPIO_WritePin(trig_port, trig_pin, GPIO_PIN_RESET);
 
@@ -37,26 +44,26 @@ int hcsr04_trigger(Hcsr04Control *hcsr04)
     return 1;
 }
 
-uint32_t hcsr04_get_dist(Hcsr04Control *hcsr04)
+uint32_t hcsr04_get_dist_sync(Hcsr04Control *hcsr04)
 {
-    uint32_t startms = 0;
-    int32_t startval = 0;
-    uint32_t endms = 0;
-    int32_t endval = 0;
+    uint32_t begin_ms = 0;
+    uint32_t begin_tick = 0;
+    uint32_t end_ms = 0;
+    uint32_t end_tick = 0;
     uint32_t timeus = 0;
     GPIO_PinState echo = GPIO_PIN_RESET;
 
     /* 发射超声波 */
     hcsr04_trigger(hcsr04);
 
-    /* 等待 ECHO 引脚返回高电平，最多等待1毫秒 */
-    startms = HAL_GetTick();
+    /* 等待 ECHO 引脚返回高电平 */
+    begin_ms = HAL_GetTick();
     do
     {
         echo = HAL_GPIO_ReadPin(hcsr04->echo_port, hcsr04->echo_pin);
-        endms = HAL_GetTick();
-        startval = SysTick->VAL;
-    } while ((GPIO_PIN_RESET == echo) && (endms < startms + 1));
+        begin_tick = SysTick->VAL;
+        end_ms = HAL_GetTick();
+    } while ((GPIO_PIN_RESET == echo) && (end_ms <= begin_ms + hcsr04->timeout));
 
     if (GPIO_PIN_RESET == echo)
     {
@@ -65,12 +72,103 @@ uint32_t hcsr04_get_dist(Hcsr04Control *hcsr04)
     }
 
     /* 检测到 ECHO 高电平，等待高电平结束，计算持续时间 */
-    startms = endms;
-    while (GPIO_PIN_SET == HAL_GPIO_ReadPin(hcsr04->echo_port, hcsr04->echo_pin));
-    endval = SysTick->VAL;
-    endms = HAL_GetTick();
-    timeus = (endms - startms) * 1000 + (startval - endval) / 72;
+    begin_ms = end_ms;
+    do
+    {
+        echo = HAL_GPIO_ReadPin(hcsr04->echo_port, hcsr04->echo_pin);
+        end_tick = SysTick->VAL;
+        end_ms = HAL_GetTick();
+    } while ((GPIO_PIN_SET == echo) && (end_ms <= begin_ms + HCSR04_TIMEOUT));
+
+    if (GPIO_PIN_SET == echo)
+    {
+        /* 超时，障碍物距离超量程 */
+        return HCSR04_INVALID_DISTANCE;
+    }
+
+    /* ECHO 高电平持续时间 */
+    timeus = (end_ms - begin_ms) * 1000
+        + ((int32_t)begin_tick - (int32_t)end_tick) / 72;
 
     /* 根据超声波在空气中的传播速度计算障碍物距离，单位毫米 */
     return timeus * 170 / 1000;
 }
+
+/**
+ * ECHO 引脚上升沿触发中断.
+ *
+ * 记录开始时间和 SysTick 寄存器当前值，用于后面计算持续持续时间.
+ */
+void hcsr04_echo_begin(Hcsr04Control *hcsr04)
+{
+    hcsr04->state = HCSR04_ECHO_BEGIN;
+    hcsr04->echo_begin_tick = SysTick->VAL;
+    hcsr04->echo_begin_ms = HAL_GetTick();
+}
+
+/**
+ * ECHO 引脚下降沿触发中断.
+ *
+ * 记录结束时间和 SysTick 寄存器当前值.
+ */
+void hcsr04_echo_end(Hcsr04Control *hcsr04)
+{
+    hcsr04->state = HCSR04_ECHO_END;
+    hcsr04->echo_end_tick = SysTick->VAL;
+    hcsr04->echo_end_ms = HAL_GetTick();
+}
+
+uint32_t hcsr04_read(Hcsr04Control *hcsr04)
+{
+    uint32_t timeus = 0;
+
+    if (hcsr04->timeout > 0)
+    {
+        /* 设置了超时时间，尝试等待 ECHO 信号结束 */
+        uint32_t begin = HAL_GetTick();
+        while ((hcsr04->state != HCSR04_ECHO_END)
+            && (HAL_GetTick() <= begin + hcsr04->timeout));
+    }
+
+    /* 未接收到 ECHO 信号或信号未结束 */
+    if ((hcsr04->state != HCSR04_ECHO_END)
+        /* 距离上次接收到 ECHO 信号已超过指定时间 */
+        || (HAL_GetTick() > hcsr04->echo_end_ms + hcsr04->timeout))
+    {
+        return HCSR04_INVALID_DISTANCE;
+    }
+
+    /* 根据记录的 ECHO 开始和结束时间计算持续时间，前者是中断设置的 */
+    timeus = (hcsr04->echo_end_ms - hcsr04->echo_begin_ms) * 1000
+        + ((int32_t)hcsr04->echo_begin_tick - (int32_t)hcsr04->echo_end_tick) / 72;
+
+    /* 根据超声波在空气中的传播速度计算障碍物距离，单位毫米 */
+    return timeus * 170 / 1000;
+}
+
+uint32_t hcsr04_get_dist(Hcsr04Control *hcsr04)
+{
+    uint32_t timeus = 0;
+    uint32_t begin_tick = SysTick->VAL;
+    uint32_t begin_ms = HAL_GetTick();
+
+    /* 发射超声波 */
+    hcsr04_trigger(hcsr04);
+
+    /* 等待 ECHO 信号结束，结束时间等由中断设置 */
+    while (((hcsr04->echo_end_ms < begin_ms)
+        || ((hcsr04->echo_end_ms == begin_ms) && (hcsr04->echo_end_tick >= begin_tick)))
+        && (HAL_GetTick() <= begin_ms + hcsr04->timeout + HCSR04_TIMEOUT));
+
+    if ((hcsr04->echo_end_ms < begin_ms)
+        || ((hcsr04->echo_end_ms == begin_ms) && (hcsr04->echo_end_tick >= begin_tick)))
+    {
+        return HCSR04_INVALID_DISTANCE;
+    }
+
+    timeus = (hcsr04->echo_end_ms - hcsr04->echo_begin_ms) * 1000
+        + ((int32_t)hcsr04->echo_begin_tick - (int32_t)hcsr04->echo_end_tick) / 72;
+
+    return timeus * 170 / 1000;
+}
+
--- lib/hcsr04.h
+++ lib/hcsr04.h
@@ -9,26 +9,54 @@
 
 #include "stm32f1xx_hal.h"
 
+
+/** 等待 ECHO 结束的超时时间，单位毫秒，根据 HC-SR04 量程估算得出 */
+#define HCSR04_TIMEOUT  20
+
+/** 根据超时时间计算得出的理论最大量程，通常大于真实有效量程 */
+#define HCSR04_MAX_DISTANCE ((HCSR04_TIMEOUT + 1) * 170 / 1000)
+
 /** 检测不到障碍物时返回的无效距离 */
 #define HCSR04_INVALID_DISTANCE UINT32_MAX
 
+/**
+ * HC-SR04 状态.
+ */
+typedef enum
+{
+    HCSR04_RESET = 0,   /**< 初始化完成，未执行任何操作 */
+    HCSR04_TRIGGER,     /**< 已发送超声波，但未接收到 ECHO 信号 */
+    HCSR04_ECHO_BEGIN,  /**< 已接收到 ECHO 信号，但信号未结束 */
+    HCSR04_ECHO_END,
+} Hcsr04State;
+
+/**
+ * HC-SR04 控制数据结构.
+ */
 typedef struct
 {
-    GPIO_TypeDef *trig_port;    /**< TRIG 端口 */
-    uint16_t trig_pin;          /**< TRIG 引脚 */
-    GPIO_TypeDef *echo_port;    /**< ECHO 端口 */
-    uint16_t echo_pin;          /**< ECHO 引脚 */
+    GPIO_TypeDef *trig_port;            /**< TRIG 端口 */
+    uint16_t trig_pin;                  /**< TRIG 引脚 */
+    GPIO_TypeDef *echo_port;            /**< ECHO 端口 */
+    uint16_t echo_pin;                  /**< ECHO 引脚 */
+    uint32_t timeout;                   /**< 读取距离时的最大等待时间 */
+    volatile Hcsr04State state;         /**< 传感器当前状态 */
+    volatile uint32_t echo_begin_ms;    /**< ECHO 引脚高电平开始时间，毫秒 */
+    volatile uint32_t echo_begin_tick;  /**< ECHO 引脚高电平开始时 SysTick 寄存器值 */
+    volatile uint32_t echo_end_ms;      /**< ECHO 引脚高电平结束时间，毫秒 */
+    volatile uint32_t echo_end_tick;    /**< ECHO 引脚高电平结束时 SysTick 寄存器值 */
 } Hcsr04Control;
 
 /**
- * 初始化接线.
+ * 初始化数据结构.
  */
 extern int hcsr04_init(
     Hcsr04Control *hcsr04,
     GPIO_TypeDef *trig_port,
     uint16_t trig_pin,
     GPIO_TypeDef *echo_port,
-    uint16_t echo_pin
+    uint16_t echo_pin,
+    uint32_t timeout
 );
 
 /**
@@ -37,7 +65,33 @@ extern int hcsr04_init(
 extern int hcsr04_trigger(Hcsr04Control *hcsr04);
 
 /**
- * 获取前方障碍物距离.
+ * 同步测量障碍物距离.
+ *
+ * 发射超声波，然后阻塞等待 ECHO 信号结束，返回距离
+ */
+extern uint32_t hcsr04_get_dist_sync(Hcsr04Control *hcsr04);
+
+/**
+ * ECHO 上升沿触发中断，记录 ECHO 信号开始时间.
+ */
+extern void hcsr04_echo_begin(Hcsr04Control *hcsr04);
+
+/**
+ * ECHO 下降沿触发中断，记录 ECHO 信号结束时间.
+ */
+extern void hcsr04_echo_end(Hcsr04Control *hcsr04);
+
+/**
+ * 异步获取前方障碍物距离.
+ *
+ * 不发射超声波，也不检测 ECHO 信号，只根据中断记录的 ECHO 时间计算上次测量的距离.
+ */
+extern uint32_t hcsr04_read(Hcsr04Control *hcsr04);
+
+/**
+ * 发射超声波并获取距离.
+ *
+ * 保证获取的距离是在触发之后测得的.
  */
 extern uint32_t hcsr04_get_dist(Hcsr04Control *hcsr04);
 
