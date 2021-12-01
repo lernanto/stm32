f61cd213a7536758e1f3b319ff28ff636b268361
黄艺华 <lernanto.wong@gmail.com>
2021-12-01 11:08:16 +0800

    优化 HC-SR04 代码

--- lib/hcsr04.h
+++ lib/hcsr04.h
@@ -10,11 +10,17 @@
 #include "stm32f1xx_hal.h"
 
 
-/** 等待 ECHO 结束的超时时间，单位毫秒，根据 HC-SR04 量程估算得出 */
-#define HCSR04_TIMEOUT  20
+/** 超声波在空气中的传播速度，单位 mm/ms */
+#define HCSR04_ULTRASOUND_SPEED 340
 
-/** 根据超时时间计算得出的理论最大量程，通常大于真实有效量程 */
-#define HCSR04_MAX_DISTANCE ((HCSR04_TIMEOUT + 1) * 170 / 1000)
+/** HC-SR04 的最小量程，单位毫米 */
+#define HCSR04_MIN_DISTANCE 20
+
+/** HC-SR04 的最大量程，单位毫米 */
+#define HCSR04_MAX_DISTANCE 4000
+
+/** 等待 ECHO 结束的超时时间，单位毫秒，根据 HC-SR04 量程估算得出 */
+#define HCSR04_TIMEOUT  (HCSR04_MAX_DISTANCE * 2 / HCSR04_ULTRASOUND_SPEED + 2)
 
 /** 检测不到障碍物时返回的无效距离 */
 #define HCSR04_INVALID_DISTANCE UINT32_MAX
@@ -39,7 +45,7 @@ typedef struct
     uint16_t trig_pin;                  /**< TRIG 引脚 */
     GPIO_TypeDef *echo_port;            /**< ECHO 端口 */
     uint16_t echo_pin;                  /**< ECHO 引脚 */
-    uint32_t timeout;                   /**< 读取距离时的最大等待时间 */
+    uint32_t expire;                    /**< 测量结果过期时间 */
     volatile Hcsr04State state;         /**< 传感器当前状态 */
     volatile uint32_t echo_begin_ms;    /**< ECHO 引脚高电平开始时间，毫秒 */
     volatile uint32_t echo_begin_tick;  /**< ECHO 引脚高电平开始时 SysTick 寄存器值 */
@@ -56,7 +62,7 @@ extern int hcsr04_init(
     uint16_t trig_pin,
     GPIO_TypeDef *echo_port,
     uint16_t echo_pin,
-    uint32_t timeout
+    uint32_t expire
 );
 
 /**
@@ -86,13 +92,18 @@ extern void hcsr04_echo_end(Hcsr04Control *hcsr04);
  *
  * 不发射超声波，也不检测 ECHO 信号，只根据中断记录的 ECHO 时间计算上次测量的距离.
  */
-extern uint32_t hcsr04_read(Hcsr04Control *hcsr04);
+extern uint32_t hcsr04_read_nowait(const Hcsr04Control *hcsr04);
+
+/**
+ * 获取障碍物的距离，如果正在接收 ECHO 信号，等待信号结束再计算.
+ */
+extern uint32_t hcsr04_read(const Hcsr04Control *hcsr04);
 
 /**
  * 发射超声波并获取距离.
  *
  * 保证获取的距离是在触发之后测得的.
  */
-extern uint32_t hcsr04_get_dist(Hcsr04Control *hcsr04);
+extern uint32_t hcsr04_get_dist_async(Hcsr04Control *hcsr04);
 
 #endif  /* _HCSR04_H */
