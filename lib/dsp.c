e353b5d181e75ce6a2b1bd95b1c23c0fcf87a38b
黄艺华 <lernanto.wong@gmail.com>
2021-12-17 10:41:54 +0800

    优化线性回归代码，添加简便函数支持常用情况

--- lib/dsp.c
+++ lib/dsp.c
@@ -11,35 +11,29 @@
 #include "log.h"
 
 
-int dsp_lwlr_init(
+int dsp_linear_regression_init(
     size_t feature_num,
-    size_t label_num,
-    float32_t *var,
+    size_t target_num,
     float32_t *xwx,
     float32_t *ywx
 )
 {
-    *var = 0.0f;
     arm_fill_f32(0.0f, xwx, feature_num * feature_num);
-    arm_fill_f32(0.0f, ywx, label_num * feature_num);
+    arm_fill_f32(0.0f, ywx, target_num * feature_num);
     return 1;
 }
 
-int dsp_lwlr_add_sample(
+int dsp_linear_regression_add_sample(
     size_t feature_num,
-    size_t label_num,
+    size_t target_num,
     const float32_t *x,
     const float32_t *y,
-    float32_t decay,
-    float32_t *var,
+    float32_t weight,
     float32_t *xwx,
     float32_t *ywx
 )
 {
-    float32_t w;
-
-    *var = *var * decay + 1;
-    w = 1.0f / *var;
+    float32_t var = 1.0f / weight - 1.0f;
 
     /* TODO: 使用 DSP 指令优化向量运算速度 */
     for (size_t i = 0, base = 0; i < feature_num; ++i, base += feature_num)
@@ -47,24 +41,24 @@ int dsp_lwlr_add_sample(
         /* XWX 是对称阵，只计算其中一半 */
         for (size_t j = i; j < feature_num; ++j)
         {
-            xwx[base + j] = (1 - w) * xwx[base + j]
-                + w * x[i] * x[j];
+            xwx[base + j] = (1 - weight) * xwx[base + j]
+                + weight * x[i] * x[j];
         }
     }
 
-    if (1 == label_num)
+    if (1 == target_num)
     {
-        /* 当 y 为一维时，使用向量运算加快速度，注意 1 - w = w * y * ((var - 1) / y) */
-        arm_scale_f32(ywx, (*var - 1.0f) / *y, ywx, feature_num);
+        /* 当 y 为一维时，使用向量运算加快速度，注意 1 - w = w * y * ((1 / w - 1) / y) */
+        arm_scale_f32(ywx, var / *y, ywx, feature_num);
         arm_add_f32(ywx, x, ywx, feature_num);
-        arm_scale_f32(ywx, w * *y, ywx, feature_num);
+        arm_scale_f32(ywx, weight * *y, ywx, feature_num);
     }
     else
     {
-        /* 注意 1 - w = w * (var - 1) */
-        arm_scale_f32(ywx, (*var - 1.0f), ywx, label_num * feature_num);
+        /* 注意 1 - w = w * (1 / w - 1) */
+        arm_scale_f32(ywx, var, ywx, target_num * feature_num);
 
-        for (size_t j = 0, base = 0; j < label_num; ++j, base += feature_num)
+        for (size_t j = 0, base = 0; j < target_num; ++j, base += feature_num)
         {
             for (size_t i = 0; i < feature_num; ++i)
             {
@@ -72,24 +66,23 @@ int dsp_lwlr_add_sample(
             }
         }
 
-        arm_scale_f32(ywx, w, ywx, label_num * feature_num);
+        arm_scale_f32(ywx, weight, ywx, target_num * feature_num);
     }
 
     return 1;
 }
 
-int dsp_lwlr_solve(
+int dsp_linear_regression_solve(
     size_t feature_num,
-    size_t label_num,
+    size_t target_num,
     float32_t *xwx,
     const float32_t *ywx,
     float32_t l2,
+    float32_t *param_buf,
+    float32_t *param_inv,
     float32_t *coef
 )
 {
-    static float32_t a_data[100];
-    static float32_t a_inv_data[100];
-
     arm_matrix_instance_f32 a;
     arm_matrix_instance_f32 a_inv;
     arm_status status = ARM_MATH_SUCCESS;
@@ -107,12 +100,12 @@ int dsp_lwlr_solve(
     /* 计算 XWX + l2 * I */
     if (l2 > 0.0f)
     {
-        arm_copy_f32(xwx, a_data, feature_num * feature_num);
+        arm_copy_f32(xwx, param_buf, feature_num * feature_num);
         for (size_t i = 0; i < feature_num * feature_num; i += feature_num + 1)
         {
-            a_data[i] += l2;
+            param_buf[i] += l2;
         }
-        arm_mat_init_f32(&a, feature_num, feature_num, a_data);
+        arm_mat_init_f32(&a, feature_num, feature_num, param_buf);
     }
     else
     {
@@ -120,7 +113,7 @@ int dsp_lwlr_solve(
     }
 
     /* 求 XWX + l2 * I 的逆 */
-    arm_mat_init_f32(&a_inv, feature_num, feature_num, a_inv_data);
+    arm_mat_init_f32(&a_inv, feature_num, feature_num, param_inv);
     if ((status = arm_mat_inverse_f32(&a, &a_inv)) != ARM_MATH_SUCCESS)
     {
         log_error("cannot inverse XWX");
@@ -128,7 +121,7 @@ int dsp_lwlr_solve(
     }
 
     /* yWX * (XWX + l2 * I) ^ -1 即为方程的解 */
-    if (1 == label_num)
+    if (1 == target_num)
     {
         /* 当 yWX 为向量时使用矩阵乘向量方式 */
         arm_mat_vec_mult_f32(&a_inv, ywx, coef);
@@ -139,8 +132,8 @@ int dsp_lwlr_solve(
         arm_matrix_instance_f32 b;
         arm_matrix_instance_f32 x;
 
-        arm_mat_init_f32(&b, label_num, feature_num, (float32_t *)ywx);
-        arm_mat_init_f32(&x, label_num, feature_num, coef);
+        arm_mat_init_f32(&b, target_num, feature_num, (float32_t *)ywx);
+        arm_mat_init_f32(&x, target_num, feature_num, coef);
         arm_mat_mult_f32(&b, &a_inv, &x);
     }
 
@@ -170,15 +163,15 @@ int dsp_lwlr_solve(
     return 1;
 }
 
-int dsp_lwlr_predict(
+int dsp_linear_regression_predict(
     size_t feature_num,
-    size_t label_num,
+    size_t target_num,
     const float32_t *coef,
     const float32_t *x,
     float32_t *y
 )
 {
-    if (1 == label_num)
+    if (1 == target_num)
     {
         arm_dot_prod_f32(coef, x, feature_num, y);
     }
@@ -186,9 +179,86 @@ int dsp_lwlr_predict(
     {
         arm_matrix_instance_f32 coef_mat;
 
-        arm_mat_init_f32(&coef_mat, label_num, feature_num, (float32_t *)coef);
+        arm_mat_init_f32(&coef_mat, target_num, feature_num, (float32_t *)coef);
         arm_mat_vec_mult_f32(&coef_mat, x, y);
     }
 
     return 1;
+}
+
+int dsp_linear_regression1_init(
+    float32_t *ex,
+    float32_t *ey,
+    float32_t *ex2,
+    float32_t *exy
+)
+{
+    *ex = 0.0f;
+    *ey = 0.0f;
+    *ex2 = 0.0f;
+    *exy = 0.0f;
+
+    return 1;
+}
+
+int dsp_linear_regression1_add_sample(
+    float32_t x,
+    float32_t y,
+    float32_t weight,
+    float32_t *ex,
+    float32_t *ey,
+    float32_t *ex2,
+    float32_t *exy
+)
+{
+    float32_t decay = 1.0f - weight;
+
+    *ey = decay * *ey + weight * y;
+    *ex2 = decay * *ex2 + weight * x * x;
+    *exy = decay * *exy + weight * x * y;
+
+    return 1;
+}
+
+int dsp_linear_regression1_solve(
+    float32_t ex,
+    float32_t ey,
+    float32_t ex2,
+    float32_t exy,
+    float32_t l2,
+    float32_t *coef,
+    float32_t *bias
+)
+{
+    float32_t det = (ex2 + l2) * (1.0f + l2) - ex * ex;
+
+    if ((det < -1e-6f) || (det > 1e-6f))
+    {
+        *coef = (exy * (1.0f + l2) - ex * ey) / det;
+        *bias = ey - *coef * ex;
+    }
+    else
+    {
+        log_debug("matrix is singular, det = %f", det);
+        *coef = 0.0f;
+        *bias = ey;
+    }
+
+    return 1;
+}
+
+int dsp_linear_regression1_predict(
+    float32_t coef,
+    float32_t bias,
+    float32_t x,
+    float32_t *y
+)
+{
+    *y = coef * x + bias;
+    return 1;
+}
+
+float32_t dsp_linear_regression_decay(float32_t var, float32_t decay)
+{
+    return var * decay + 1;
 }
--- lib/dsp.h
+++ lib/dsp.h
@@ -9,40 +9,76 @@
 #include "arm_math.h"
 
 
-extern int dsp_lwlr_init(
+extern int dsp_linear_regression_init(
     size_t feature_num,
-    size_t label_num,
-    float32_t *var,
+    size_t target_num,
     float32_t *xwx,
     float32_t *ywx
 );
 
-extern int dsp_lwlr_add_sample(
+extern int dsp_linear_regression_add_sample(
     size_t feature_num,
-    size_t label_num,
+    size_t target_num,
     const float32_t *x,
     const float32_t *y,
-    float32_t decay,
-    float32_t *var,
+    float32_t weight,
     float32_t *xwx,
     float32_t *ywx
 );
 
-extern int dsp_lwlr_solve(
+extern int dsp_linear_regression_solve(
     size_t feature_num,
-    size_t label_num,
+    size_t target_num,
     float32_t *xwx,
     const float32_t *ywx,
     float32_t l2,
+    float32_t *param_buf,
+    float32_t *param_inv,
     float32_t *coef
 );
 
-extern int dsp_lwlr_predict(
+extern int dsp_linear_regression_predict(
     size_t feature_num,
-    size_t label_num,
+    size_t target_num,
     const float32_t *coef,
     const float32_t *x,
     float32_t *y
 );
+
+extern int dsp_linear_regression1_init(
+    float32_t *ex,
+    float32_t *ey,
+    float32_t *ex2,
+    float32_t *exy
+);
+
+extern int dsp_linear_regression1_add_sample(
+    float32_t x,
+    float32_t y,
+    float32_t weight,
+    float32_t *ex,
+    float32_t *ey,
+    float32_t *ex2,
+    float32_t *exy
+);
+
+extern int dsp_linear_regression1_solve(
+    float32_t ex,
+    float32_t ey,
+    float32_t ex2,
+    float32_t exy,
+    float32_t l2,
+    float32_t *coef,
+    float32_t *bias
+);
+
+extern int dsp_linear_regression1_predict(
+    float32_t coef,
+    float32_t bias,
+    float32_t x,
+    float32_t *y
+);
+
+extern float32_t dsp_linear_regression_decay(float32_t var, float32_t decay);
 
 #endif  /* _DSP_H */
