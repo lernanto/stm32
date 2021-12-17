54952f7beefb51177ccd0faabeea60586f6d6c14
黄艺华 <lernanto.wong@gmail.com>
2021-12-17 16:12:51 +0800

    添加线性回归注释

--- lib/dsp.c
+++ lib/dsp.c
@@ -1,5 +1,5 @@
 /**
- * @brief 
+ * @brief 数字信号处理功能实现
  * @author 黄艺华 (lernanto@foxmail.com)
  */
 
@@ -12,20 +12,20 @@
 
 
 int dsp_linear_regression_init(
-    size_t feature_num,
-    size_t target_num,
+    size_t feature_dim,
+    size_t target_dim,
     float32_t *xwx,
     float32_t *ywx
 )
 {
-    arm_fill_f32(0.0f, xwx, feature_num * feature_num);
-    arm_fill_f32(0.0f, ywx, target_num * feature_num);
+    arm_fill_f32(0.0f, xwx, feature_dim * feature_dim);
+    arm_fill_f32(0.0f, ywx, target_dim * feature_dim);
     return 1;
 }
 
 int dsp_linear_regression_add_sample(
-    size_t feature_num,
-    size_t target_num,
+    size_t feature_dim,
+    size_t target_dim,
     const float32_t *x,
     const float32_t *y,
     float32_t weight,
@@ -36,45 +36,45 @@ int dsp_linear_regression_add_sample(
     float32_t var = 1.0f / weight - 1.0f;
 
     /* TODO: 使用 DSP 指令优化向量运算速度 */
-    for (size_t i = 0, base = 0; i < feature_num; ++i, base += feature_num)
+    for (size_t i = 0, base = 0; i < feature_dim; ++i, base += feature_dim)
     {
         /* XWX 是对称阵，只计算其中一半 */
-        for (size_t j = i; j < feature_num; ++j)
+        for (size_t j = i; j < feature_dim; ++j)
         {
             xwx[base + j] = (1 - weight) * xwx[base + j]
                 + weight * x[i] * x[j];
         }
     }
 
-    if (1 == target_num)
+    if (1 == target_dim)
     {
         /* 当 y 为一维时，使用向量运算加快速度，注意 1 - w = w * y * ((1 / w - 1) / y) */
-        arm_scale_f32(ywx, var / *y, ywx, feature_num);
-        arm_add_f32(ywx, x, ywx, feature_num);
-        arm_scale_f32(ywx, weight * *y, ywx, feature_num);
+        arm_scale_f32(ywx, var / *y, ywx, feature_dim);
+        arm_add_f32(ywx, x, ywx, feature_dim);
+        arm_scale_f32(ywx, weight * *y, ywx, feature_dim);
     }
     else
     {
         /* 注意 1 - w = w * (1 / w - 1) */
-        arm_scale_f32(ywx, var, ywx, target_num * feature_num);
+        arm_scale_f32(ywx, var, ywx, target_dim * feature_dim);
 
-        for (size_t j = 0, base = 0; j < target_num; ++j, base += feature_num)
+        for (size_t j = 0, base = 0; j < target_dim; ++j, base += feature_dim)
         {
-            for (size_t i = 0; i < feature_num; ++i)
+            for (size_t i = 0; i < feature_dim; ++i)
             {
                 ywx[base + i] += x[i] * y[j];
             }
         }
 
-        arm_scale_f32(ywx, weight, ywx, target_num * feature_num);
+        arm_scale_f32(ywx, weight, ywx, target_dim * feature_dim);
     }
 
     return 1;
 }
 
 int dsp_linear_regression_solve(
-    size_t feature_num,
-    size_t target_num,
+    size_t feature_dim,
+    size_t target_dim,
     float32_t *xwx,
     const float32_t *ywx,
     float32_t l2,
@@ -89,9 +89,9 @@ int dsp_linear_regression_solve(
 
     /* 解线性方程组 (XWX + l2 * I) * coef = yWX */
     /* 对称阵 XWX 只计算了一半，先复制补全对称的另一半 */
-    for (size_t i = 0, base = 0; i < feature_num; ++i, base += feature_num)
+    for (size_t i = 0, base = 0; i < feature_dim; ++i, base += feature_dim)
     {
-        for (size_t j = base, k = i; j < base + i; ++j, k += feature_num)
+        for (size_t j = base, k = i; j < base + i; ++j, k += feature_dim)
         {
             xwx[j] = xwx[k];
         }
@@ -100,20 +100,20 @@ int dsp_linear_regression_solve(
     /* 计算 XWX + l2 * I */
     if (l2 > 0.0f)
     {
-        arm_copy_f32(xwx, param_buf, feature_num * feature_num);
-        for (size_t i = 0; i < feature_num * feature_num; i += feature_num + 1)
+        arm_copy_f32(xwx, param_buf, feature_dim * feature_dim);
+        for (size_t i = 0; i < feature_dim * feature_dim; i += feature_dim + 1)
         {
             param_buf[i] += l2;
         }
-        arm_mat_init_f32(&a, feature_num, feature_num, param_buf);
+        arm_mat_init_f32(&a, feature_dim, feature_dim, param_buf);
     }
     else
     {
-        arm_mat_init_f32(&a, feature_num, feature_num, (float32_t *)xwx);
+        arm_mat_init_f32(&a, feature_dim, feature_dim, (float32_t *)xwx);
     }
 
     /* 求 XWX + l2 * I 的逆 */
-    arm_mat_init_f32(&a_inv, feature_num, feature_num, param_inv);
+    arm_mat_init_f32(&a_inv, feature_dim, feature_dim, param_inv);
     if ((status = arm_mat_inverse_f32(&a, &a_inv)) != ARM_MATH_SUCCESS)
     {
         log_error("cannot inverse XWX");
@@ -121,7 +121,7 @@ int dsp_linear_regression_solve(
     }
 
     /* yWX * (XWX + l2 * I) ^ -1 即为方程的解 */
-    if (1 == target_num)
+    if (1 == target_dim)
     {
         /* 当 yWX 为向量时使用矩阵乘向量方式 */
         arm_mat_vec_mult_f32(&a_inv, ywx, coef);
@@ -132,8 +132,8 @@ int dsp_linear_regression_solve(
         arm_matrix_instance_f32 b;
         arm_matrix_instance_f32 x;
 
-        arm_mat_init_f32(&b, target_num, feature_num, (float32_t *)ywx);
-        arm_mat_init_f32(&x, target_num, feature_num, coef);
+        arm_mat_init_f32(&b, target_dim, feature_dim, (float32_t *)ywx);
+        arm_mat_init_f32(&x, target_dim, feature_dim, coef);
         arm_mat_mult_f32(&b, &a_inv, &x);
     }
 
@@ -164,22 +164,22 @@ int dsp_linear_regression_solve(
 }
 
 int dsp_linear_regression_predict(
-    size_t feature_num,
-    size_t target_num,
+    size_t feature_dim,
+    size_t target_dim,
     const float32_t *coef,
     const float32_t *x,
     float32_t *y
 )
 {
-    if (1 == target_num)
+    if (1 == target_dim)
     {
-        arm_dot_prod_f32(coef, x, feature_num, y);
+        arm_dot_prod_f32(coef, x, feature_dim, y);
     }
     else
     {
         arm_matrix_instance_f32 coef_mat;
 
-        arm_mat_init_f32(&coef_mat, target_num, feature_num, (float32_t *)coef);
+        arm_mat_init_f32(&coef_mat, target_dim, feature_dim, (float32_t *)coef);
         arm_mat_vec_mult_f32(&coef_mat, x, y);
     }
 
@@ -256,9 +256,4 @@ int dsp_linear_regression1_predict(
 {
     *y = coef * x + bias;
     return 1;
-}
-
-float32_t dsp_linear_regression_decay(float32_t var, float32_t decay)
-{
-    return var * decay + 1;
 }
--- lib/dsp.h
+++ lib/dsp.h
@@ -1,6 +1,16 @@
 /**
- * @brief 信号处理功能函数
+ * @brief 数字信号处理功能函数
  * @author 黄艺华 (lernanto@foxmail.com)
+ *
+ * 当前只包含增量线性回归功能
+ * 增量是指每次读入一条样本，读入任意数量的样本后，都可以求解线性回归模型，
+ * 解出的模型考虑了迄今为止读入的所有样本。继续读入新的样本，重复求解过程可以得到更新的模型，
+ * 这个过程可以一直持续下去。这种求解过程适合样本随着时间采集，采集的同时又要即时求解的情况。
+ *
+ * 读入样本的时候可以为样本指定权重
+ *  - 如指定权重 w = 1 / n，其中 n 为迄今为止读入的样本数量（包括当前样本），则相当于普通线性回归
+ *  - 指定 w = 1 / (1 / w * d + 1)，其中 d 为 (0, 1] 的衰减系数，相当于局部加权线性回归
+ *  - 指定任意 w 属于 (0, 1)，相当于普通加权线性回归，w 需要根据样本权重预先计算
  */
 
 #ifndef _DSP_H
@@ -9,16 +19,38 @@
 #include "arm_math.h"
 
 
+/**
+ * @brief 初始化线性回归模型
+ *
+ * @param feature_dim 样本特征向量维数
+ * @param target_dim 预测的目标向量维数
+ * @param xwx 增量求解线性回归需要保存的统计量矩阵，X^T * W * X
+ * @param ywx 增量求解线性回归需要保存的统计量矩阵，Y^T * W * X
+ * @return 成功返回非0，失败返回0
+ */
 extern int dsp_linear_regression_init(
-    size_t feature_num,
-    size_t target_num,
+    size_t feature_dim,
+    size_t target_dim,
     float32_t *xwx,
     float32_t *ywx
 );
 
+/**
+ * @brief 向线性回归读入一个样本
+ *
+ * @param feature_dim 样本特征向量维数
+ * @param target_dim 预测的目标向量维数
+ * @param x 样本特征
+ * @param y 样本目标
+ * @param weight 样本权重，(0, 1)
+ * 先前样本的权重会由于加入更多样本而衰减，所以这个权重并不等于加权线性回归的权重
+ * @param xwx 先前保存的统计量矩阵，读入样本后会更新
+ * @param ywx 先前保存的统计量矩阵，读入样本后会更新
+ * @return 成功返回非0，失败返回0
+ */
 extern int dsp_linear_regression_add_sample(
-    size_t feature_num,
-    size_t target_num,
+    size_t feature_dim,
+    size_t target_dim,
     const float32_t *x,
     const float32_t *y,
     float32_t weight,
@@ -26,9 +58,22 @@ extern int dsp_linear_regression_add_sample(
     float32_t *ywx
 );
 
+/**
+ * @brief 根据迄今为止读入的样本，求解线性回归模型
+ *
+ * @param feature_dim 样本特征向量维数
+ * @param target_dim 预测的目标向量维数
+ * @param xwx 先前保存的统计量矩阵
+ * @param ywx 先前保存的统计量矩阵
+ * @param l2 L2 正则化系数，[0, +inf)
+ * @param param_buf 由调用者提供，供临时存储中间结果矩阵的空间，当 l2 = 0 时可以为 NULL
+ * @param param_inv 保存 (X^T * W * X)^-1 的计算结果
+ * @param coef 保存求解得到的模型系数
+ * @return 成功返回非0，失败返回0
+ */
 extern int dsp_linear_regression_solve(
-    size_t feature_num,
-    size_t target_num,
+    size_t feature_dim,
+    size_t target_dim,
     float32_t *xwx,
     const float32_t *ywx,
     float32_t l2,
@@ -37,14 +82,35 @@ extern int dsp_linear_regression_solve(
     float32_t *coef
 );
 
+/**
+ * @brief 使用解得的线性回归模型预测目标
+ *
+ * @param feature_dim 样本特征向量维数
+ * @param target_dim 预测的目标向量维数
+ * @param coef 线性回归模型系数
+ * @param x 输入特征向量
+ * @param y 保存预测得到的目标向量
+ * @return 成功返回非0，失败返回0
+ */
 extern int dsp_linear_regression_predict(
-    size_t feature_num,
-    size_t target_num,
+    size_t feature_dim,
+    size_t target_dim,
     const float32_t *coef,
     const float32_t *x,
     float32_t *y
 );
 
+/**
+ * @brief 初始化一元线性回归模型
+ *
+ * @param ex 增量求解过程中用来保存特征的期望
+ * @param ey 增量求解过程中用来保存目标的期望
+ * @param ex2 增量求解过程中用来保存 x^2 的期望
+ * @param exy 增量求解过程中用来保存 x * y 的期望
+ * @return 成功返回非0，失败返回0
+ *
+ * @note 是线性回归模型在输入和输出都是一元的情况下的简便函数
+ */
 extern int dsp_linear_regression1_init(
     float32_t *ex,
     float32_t *ey,
@@ -52,6 +118,18 @@ extern int dsp_linear_regression1_init(
     float32_t *exy
 );
 
+/**
+ * @brief 一元线性回归模型读入一个样本
+ *
+ * @param x 样本特征
+ * @param y 样本目标
+ * @param weight 样本权重
+ * @param ex 保存迄今为止 x 的期望
+ * @param ey 保存迄今为止 y 的期望
+ * @param ex2 保存迄今为止 x^2 的期望
+ * @param exy 保存迄今为止 x * y 的期望
+ * @return 成功返回非0，失败返回0
+ */
 extern int dsp_linear_regression1_add_sample(
     float32_t x,
     float32_t y,
@@ -62,6 +140,18 @@ extern int dsp_linear_regression1_add_sample(
     float32_t *exy
 );
 
+/**
+ * @brief 求解一元线性回归模型
+ *
+ * @param ex x 的期望
+ * @param ey y 的期望
+ * @param ex2 x^2 的期望
+ * @param exy x * y 的期望
+ * @param l2 L2 正则化系数，[0, +inf)
+ * @param coef 求解得到的特征系数
+ * @param bias 求解得到的常数偏置
+ * @return 成功返回非0，失败返回0
+ */
 extern int dsp_linear_regression1_solve(
     float32_t ex,
     float32_t ey,
@@ -72,6 +162,15 @@ extern int dsp_linear_regression1_solve(
     float32_t *bias
 );
 
+/**
+ * @brief 使用解得的一元线性回归模型预测目标
+ *
+ * @param coef 模型特征系数
+ * @param bias 模型常数偏置
+ * @param x 输入特征
+ * @param y 保存预测得到的目标
+ * @return 成功返回非0，失败返回0
+ */
 extern int dsp_linear_regression1_predict(
     float32_t coef,
     float32_t bias,
@@ -79,6 +178,33 @@ extern int dsp_linear_regression1_predict(
     float32_t *y
 );
 
-extern float32_t dsp_linear_regression_decay(float32_t var, float32_t decay);
+/**
+ * @brief 为局部加权线性回归更新样本权重的简便函数
+ *
+ * @return 权重倒数的初始值，和先验方差成正比
+ */
+static inline float32_t dsp_linear_regression_init_weight(void)
+{
+    return 0.0f;
+}
+
+/**
+ * @brief 根据衰减系数更新样本权重
+ *
+ * @param var 保存权重的导数，和先验方差成正比
+ * @param decay 衰减系数，(0, 1]，越大表示旧样本的权重越高，特殊取值下
+ *  - 当取0时，相当于只使用最新的一条样本
+ *  - 当取1时，相当于新旧样本同等权重
+ *  - 当取 > 1 时，相当于旧样本比新样本权重更高
+ * @return 下一条样本权重
+ */
+static inline float32_t dsp_linear_regression_update_weight(
+    float32_t *var,
+    float32_t decay
+)
+{
+    *var = *var * decay + 1.0f;
+    return 1.0f / *var;
+}
 
 #endif  /* _DSP_H */
