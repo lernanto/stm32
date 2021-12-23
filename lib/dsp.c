e05b9582f75a4ff01ea68a833d00b6ac67c076a0
黄艺华 <lernanto.wong@gmail.com>
2021-12-23 09:51:32 +0800

    优化线性回归代码

--- lib/dsp.c
+++ lib/dsp.c
@@ -3,7 +3,7 @@
  * @author 黄艺华 (lernanto@foxmail.com)
  */
 
-#include <string.h>
+#include <stdlib.h>
 
 #include "arm_math.h"
 
@@ -12,249 +12,336 @@
 
 
 int dsp_linear_regression_init(
-    size_t feature_dim,
-    size_t target_dim,
+    size_t feature_num,
+    size_t target_num,
     float32_t *xwx,
-    float32_t *ywx
+    float32_t *xwy
 )
 {
-    arm_fill_f32(0.0f, xwx, feature_dim * feature_dim);
-    arm_fill_f32(0.0f, ywx, target_dim * feature_dim);
+    ++feature_num;
+    arm_fill_f32(0.0f, xwx, feature_num * feature_num);
+    arm_fill_f32(0.0f, xwy, feature_num * target_num);
     return 1;
 }
 
-int dsp_linear_regression_add_sample(
-    size_t feature_dim,
-    size_t target_dim,
+static void update_xwx(
+    size_t feature_num,
     const float32_t *x,
-    const float32_t *y,
     float32_t weight,
-    float32_t *xwx,
-    float32_t *ywx
+    float32_t *xwx
 )
 {
-    float32_t var = 1.0f / weight - 1.0f;
+    float32_t decay = 1.0f - weight;
+    float32_t *pwx = xwx + feature_num * (feature_num + 1);
+#ifdef __GNUC__
+    float32_t wx[feature_num + 1];
+#else
+    float32_t *wx = (float32_t *)malloc(sizeof(float32_t) * (feature_num + 1));
+#endif  /* __GNUC__ */
 
     /* TODO: 使用 DSP 指令优化向量运算速度 */
-    for (size_t i = 0, base = 0; i < feature_dim; ++i, base += feature_dim)
+    for (size_t i = 0, base = 0; i < feature_num; ++i, base += feature_num + 1)
     {
-        /* XWX 是对称阵，只计算其中一半 */
-        for (size_t j = i; j < feature_dim; ++j)
+        /* XWX 是对称阵，只计算其中下三角的一半 */
+        for (size_t j = 0; j <= i; ++j)
         {
-            xwx[base + j] = (1 - weight) * xwx[base + j]
+            xwx[base + j] = decay * xwx[base + j]
                 + weight * x[i] * x[j];
         }
     }
 
-    if (1 == target_dim)
-    {
-        /* 当 y 为一维时，使用向量运算加快速度，注意 1 - w = w * y * ((1 / w - 1) / y) */
-        arm_scale_f32(ywx, var / *y, ywx, feature_dim);
-        arm_add_f32(ywx, x, ywx, feature_dim);
-        arm_scale_f32(ywx, weight * *y, ywx, feature_dim);
-    }
-    else
-    {
-        /* 注意 1 - w = w * (1 / w - 1) */
-        arm_scale_f32(ywx, var, ywx, target_dim * feature_dim);
+    /* 计算 XWX 中的常数项部分，即特征 x 的加权平均 */
+    arm_scale_f32(pwx, decay, pwx, feature_num + 1);
+    arm_scale_f32(x, weight, wx, feature_num);
+    wx[feature_num] = weight;
+    arm_add_f32(pwx, wx, pwx, feature_num + 1);
 
-        for (size_t j = 0, base = 0; j < target_dim; ++j, base += feature_dim)
-        {
-            for (size_t i = 0; i < feature_dim; ++i)
-            {
-                ywx[base + i] += x[i] * y[j];
-            }
-        }
-
-        arm_scale_f32(ywx, weight, ywx, target_dim * feature_dim);
-    }
-
-    return 1;
+#ifndef __GNUC__
+    free(wx);
+#endif  /* __GNUC__ */
 }
 
-int dsp_linear_regression_solve(
-    size_t feature_dim,
-    size_t target_dim,
-    float32_t *xwx,
-    const float32_t *ywx,
-    float32_t l2,
-    float32_t *param_buf,
-    float32_t *param_inv,
-    float32_t *coef
+static void update_xwy(
+    size_t feature_num,
+    const float32_t *x,
+    float32_t y,
+    float32_t weight,
+    float32_t *xwy
 )
 {
-    arm_matrix_instance_f32 a;
-    arm_matrix_instance_f32 a_inv;
-    arm_status status = ARM_MATH_SUCCESS;
+#ifdef __GNUC__
+    float32_t wyx[feature_num + 1];
+#else
+    float32_t *wyx = (float32_t *)malloc(sizeof(float32_t) * (feature_num + 1));
+#endif  /* __GNUC__ */
 
-    /* 解线性方程组 (XWX + l2 * I) * coef = yWX */
-    /* 对称阵 XWX 只计算了一半，先复制补全对称的另一半 */
-    for (size_t i = 0, base = 0; i < feature_dim; ++i, base += feature_dim)
+    arm_scale_f32(xwy, 1.0f - weight, xwy, feature_num + 1);
+    arm_scale_f32(x, weight * y, wyx, feature_num);
+    wyx[feature_num] = weight * y;
+    arm_add_f32(xwy, wyx, xwy, feature_num + 1);
+
+#ifndef __GNUC__
+    free(wyx);
+#endif  /* __GNUC__ */
+}
+
+static void update_xwy_mt(
+    size_t feature_num,
+    size_t target_num,
+    const float32_t *x,
+    const float32_t *y,
+    float32_t weight,
+    float32_t *xwy
+)
+{
+#ifdef __GNUC__
+    float32_t wy[target_num];
+#else
+    float32_t *wy = (float32_t *)malloc(sizeof(float32_t) * target_num);
+#endif  /* __GNUC__ */
+
+    arm_scale_f32(xwy, 1.0f - weight, xwy, (feature_num + 1) * target_num);
+
+    for (size_t i = 0, base = 0; i < feature_num; ++i, base += target_num)
     {
-        for (size_t j = base, k = i; j < base + i; ++j, k += feature_dim)
+        for (size_t j = 0; j < target_num; ++j)
         {
-            xwx[j] = xwx[k];
+            xwy[base + j] += weight * x[i] * y[j];
+        }
+    }
+
+    arm_scale_f32(y, weight, wy, target_num);
+    arm_add_f32(
+        xwy + feature_num * target_num,
+        wy,
+        xwy + feature_num * target_num,
+        target_num
+    );
+
+#ifndef __GNUC__
+    free(wy);
+#endif  /* __GNUC__ */
+}
+
+static arm_status inv(
+    size_t dim,
+    const float32_t *xwx,
+    float32_t l2,
+    arm_matrix_instance_f32 *result
+)
+{
+    arm_status status;
+    arm_matrix_instance_f32 a_mat;
+#ifdef __GNUC__
+    float32_t a[dim * dim];
+#else
+    float32_t *a = (float32_t *)malloc(sizeof(float32_t) * dim * dim);
+#endif  /* __GNUC__ */
+
+    arm_mat_init_f32(&a_mat, dim, dim, a);
+    arm_copy_f32(xwx, a, dim * dim);
+
+    /* 对称阵 XWX 只计算了下三角的一半，先复制补全对称的另一半 */
+    for (size_t i = 0; i < dim; ++i)
+    {
+        for (size_t j = 0; j < i; ++j)
+        {
+            a[j * dim + i] = a[i * dim + j];
         }
     }
 
     /* 计算 XWX + l2 * I */
     if (l2 > 0.0f)
     {
-        arm_copy_f32(xwx, param_buf, feature_dim * feature_dim);
-        for (size_t i = 0; i < feature_dim * feature_dim; i += feature_dim + 1)
+        for (size_t i = 0; i < dim * dim; i += dim + 1)
         {
-            param_buf[i] += l2;
+            a[i] += l2;
         }
-        arm_mat_init_f32(&a, feature_dim, feature_dim, param_buf);
-    }
-    else
-    {
-        arm_mat_init_f32(&a, feature_dim, feature_dim, (float32_t *)xwx);
     }
 
     /* 求 XWX + l2 * I 的逆 */
-    arm_mat_init_f32(&a_inv, feature_dim, feature_dim, param_inv);
-    if ((status = arm_mat_inverse_f32(&a, &a_inv)) != ARM_MATH_SUCCESS)
-    {
-        log_error("cannot inverse XWX");
-        return 0;
-    }
+    status = arm_mat_inverse_f32(&a_mat, result);
+#ifndef __GNUC__
+    free(a);
+#endif  /* __GNUC__ */
+    return status;
+}
 
-    /* yWX * (XWX + l2 * I) ^ -1 即为方程的解 */
-    if (1 == target_dim)
+void dsp_linear_regression_uni_update(
+    float32_t x,
+    float32_t y,
+    float32_t weight,
+    float32_t xwx[4],
+    float32_t xwy[2]
+)
+{
+    arm_scale_f32(xwx, 1.0f - weight, xwx, 4);
+    xwx[0] += weight * x * x;
+    xwx[2] += weight * x;
+    xwx[3] += weight;
+
+    arm_scale_f32(xwy, 1.0f - weight, xwy, 2);
+    xwy[0] += weight * x * y;
+    xwy[1] += weight * y;
+}
+
+int dsp_linear_regression_uni_solve(
+    const float32_t xwx[4],
+    const float32_t xwy[2],
+    float32_t l2,
+    float32_t coef[2]
+)
+{
+    /* 直接计算二元一次非齐次方程组 */
+    float32_t det = (xwx[0] + l2) * (xwx[3] + l2) - xwx[2] * xwx[2];
+
+    if (det != 0.0f)
     {
-        /* 当 yWX 为向量时使用矩阵乘向量方式 */
-        arm_mat_vec_mult_f32(&a_inv, ywx, coef);
+        coef[0] = (xwy[0] * (xwx[3] + l2) - xwx[2] * xwy[1]) / det;
+        coef[1] = xwy[1] - coef[0] * xwx[2];
+        return 1;
     }
     else
     {
-        /* 当 yWX 为矩阵时使用矩阵乘 */
-        arm_matrix_instance_f32 b;
-        arm_matrix_instance_f32 x;
-
-        arm_mat_init_f32(&b, target_dim, feature_dim, (float32_t *)ywx);
-        arm_mat_init_f32(&x, target_dim, feature_dim, coef);
-        arm_mat_mult_f32(&b, &a_inv, &x);
-    }
-
-#if 0
-    /* 对 XWX + l2 * I 进行 Cholesky 分解 */
-    if ((status = arm_mat_cholesky_f32(&a, &cholesky)) != ARM_MATH_SUCCESS)
-    {
-        log_error("cannot perform Cholesky decomposition on XWX");
+        /* 行列式为0，参数矩阵不满秩，方程组的解不唯一 */
         return 0;
     }
-
-    /* Cholesky 分解把系数矩阵分解为上三角阵及其转置的乘积，
-       求解原方程只需要求解两次系数为三角阵的方程即可 */
-    if ((status = arm_mat_solve_upper_triangular_f32(&cholesky, &b, &x)) != ARM_MATH_SUCCESS)
-    {
-        log_error("cannot solve upper triangular formula");
-        return 0;
-    }
-
-    if ((status = arm_mat_solve_lower_triangular_f32(cholesky, x, coef)) != ARM_MATH_SUCCESS)
-    {
-        log_error("cannot solve lower triangular formula");
-        return 0;
-    }
-#endif
-
-    return 1;
 }
 
-int dsp_linear_regression_predict(
-    size_t feature_dim,
-    size_t target_dim,
+float32_t dsp_linear_regression_uni_predict(
+    const float32_t *coef,
+    float32_t x
+)
+{
+    return coef[0] * x + coef[1];
+}
+
+void dsp_linear_regression_mul_update(
+    size_t feature_num,
+    const float32_t *x,
+    float32_t y,
+    float32_t weight,
+    float32_t *xwx,
+    float32_t *xwy
+)
+{
+    update_xwx(feature_num, x, weight, xwx);
+    update_xwy(feature_num, x, y, weight, xwy);
+}
+
+/**
+ * 解线性方程组 (XWX + l2 * I) * coef = XWY
+ */
+int dsp_linear_regression_mul_solve(
+    size_t feature_num,
+    const float32_t *xwx,
+    const float32_t *xwy,
+    float32_t l2,
+    float32_t *coef
+)
+{
+    arm_status status;
+    size_t dim = feature_num + 1;
+#ifdef __GNUC__
+    float32_t a_inv[dim * dim];
+#else
+    float32_t *a_inv = (float32_t *)malloc(sizeof(float32_t) * dim * dim);
+#endif  /* __GNUC__ */
+    arm_matrix_instance_f32 a_inv_mat;
+
+    arm_mat_init_f32(&a_inv_mat, dim, dim, a_inv);
+    status = inv(dim, xwx, l2, &a_inv_mat);
+    if (ARM_MATH_SUCCESS == status)
+    {
+        /* (XWX + l2 * I) ^ -1 * XWY 为方程的解，当 XWY 为向量时使用矩阵乘向量方式 */
+        arm_mat_vec_mult_f32(&a_inv_mat, xwy, coef);
+    }
+
+#ifndef __GNUC__
+    free(a_inv);
+#endif  /* __GNUC__ */
+    return ARM_MATH_SUCCESS == status;
+}
+
+float32_t dsp_linear_regression_mul_predict(
+    size_t feature_num,
+    const float32_t *coef,
+    const float32_t *x
+)
+{
+    float32_t y;
+
+    arm_dot_prod_f32(coef, x, feature_num, &y);
+    y += coef[feature_num];
+    return y;
+}
+
+void dsp_linear_regression_mt_update(
+    size_t feature_num,
+    size_t target_num,
+    const float32_t *x,
+    const float32_t *y,
+    float32_t weight,
+    float32_t *xwx,
+    float32_t *xwy
+)
+{
+    update_xwx(feature_num, x, weight, xwx);
+    update_xwy_mt(feature_num, target_num, x, y, weight, xwy);
+}
+
+int dsp_linear_regression_mt_solve(
+    size_t feature_num,
+    size_t target_num,
+    const float32_t *xwx,
+    const float32_t *xwy,
+    float32_t l2,
+    float32_t *coef
+)
+{
+    arm_status status;
+    size_t dim = feature_num + 1;
+#ifdef __GNUC__
+    float32_t a_inv[dim * dim];
+#else
+    float32_t *a_inv = (float32_t *)malloc(sizeof(float32_t) * dim * dim);
+#endif  /* __GNUC__ */
+    arm_matrix_instance_f32 a_inv_mat;
+
+    arm_mat_init_f32(&a_inv_mat, dim, dim, a_inv);
+    status = inv(dim, xwx, l2, &a_inv_mat);
+    if (ARM_MATH_SUCCESS == status)
+    {
+        /* (XWX + l2 * I) ^ -1 * XWY 即为方程的解 */
+        arm_matrix_instance_f32 xwy_mat;
+        arm_matrix_instance_f32 coef_mat;
+
+        arm_mat_init_f32(&xwy_mat, dim, target_num, (float32_t *)xwy);
+        arm_mat_init_f32(&coef_mat, dim, target_num, coef);
+        arm_mat_mult_f32(&a_inv_mat, &xwy_mat, &coef_mat);
+    }
+
+#ifndef __GNUC__
+    free(a_inv);
+#endif  /* __GNUC__ */
+    return ARM_MATH_SUCCESS == status;
+}
+
+void dsp_linear_regression_mt_predict(
+    size_t feature_num,
+    size_t target_num,
     const float32_t *coef,
     const float32_t *x,
     float32_t *y
 )
 {
-    if (1 == target_dim)
-    {
-        arm_dot_prod_f32(coef, x, feature_dim, y);
-    }
-    else
-    {
-        arm_matrix_instance_f32 coef_mat;
+    arm_matrix_instance_f32 coef_mat;
+    arm_matrix_instance_f32 x_mat;
+    arm_matrix_instance_f32 y_mat;
 
-        arm_mat_init_f32(&coef_mat, target_dim, feature_dim, (float32_t *)coef);
-        arm_mat_vec_mult_f32(&coef_mat, x, y);
-    }
-
-    return 1;
-}
-
-int dsp_linear_regression1_init(
-    float32_t *ex,
-    float32_t *ey,
-    float32_t *ex2,
-    float32_t *exy
-)
-{
-    *ex = 0.0f;
-    *ey = 0.0f;
-    *ex2 = 0.0f;
-    *exy = 0.0f;
-
-    return 1;
-}
-
-int dsp_linear_regression1_add_sample(
-    float32_t x,
-    float32_t y,
-    float32_t weight,
-    float32_t *ex,
-    float32_t *ey,
-    float32_t *ex2,
-    float32_t *exy
-)
-{
-    float32_t decay = 1.0f - weight;
-
-    *ex = decay * *ex + weight * x;
-    *ey = decay * *ey + weight * y;
-    *ex2 = decay * *ex2 + weight * x * x;
-    *exy = decay * *exy + weight * x * y;
-
-    return 1;
-}
-
-int dsp_linear_regression1_solve(
-    float32_t ex,
-    float32_t ey,
-    float32_t ex2,
-    float32_t exy,
-    float32_t l2,
-    float32_t *coef,
-    float32_t *bias
-)
-{
-    float32_t det = (ex2 + l2) * (1.0f + l2) - ex * ex;
-
-    if ((det < -1e-6f) || (det > 1e-6f))
-    {
-        *coef = (exy * (1.0f + l2) - ex * ey) / det;
-        *bias = ey - *coef * ex;
-    }
-    else
-    {
-        log_debug("matrix is singular, det = %f", det);
-        *coef = 0.0f;
-        *bias = ey;
-    }
-
-    return 1;
-}
-
-int dsp_linear_regression1_predict(
-    float32_t coef,
-    float32_t bias,
-    float32_t x,
-    float32_t *y
-)
-{
-    *y = coef * x + bias;
-    return 1;
+    arm_mat_init_f32(&coef_mat, feature_num, target_num, (float32_t *)coef);
+    arm_mat_init_f32(&x_mat, 1, feature_num, (float32_t *)x);
+    arm_mat_init_f32(&y_mat, 1, target_num, y);
+    arm_mat_mult_f32(&x_mat, &coef_mat, &y_mat);
+    arm_add_f32(y, coef + feature_num * target_num, y, target_num);
 }
--- lib/dsp.h
+++ lib/dsp.h
@@ -22,87 +22,253 @@
 /**
  * @brief 初始化线性回归模型
  *
- * @param feature_dim 样本特征向量维数
- * @param target_dim 预测的目标向量维数
- * @param xwx 增量求解线性回归需要保存的统计量矩阵，X^T * W * X
- * @param ywx 增量求解线性回归需要保存的统计量矩阵，Y^T * W * X
+ * @param feature_num 样本特征数
+ * @param target_num 预测的目标数
+ * @param xwx 增量求解线性回归需要保存的统计量矩阵，X^T * W * X，
+ * 其中 X 为增加常数项1维度后的特征矩阵，XWX 维度为 (feature_num + 1) * (feature_num + 1)
+ * @param xwy 增量求解线性回归需要保存的统计量矩阵，Y^T * W * X
+ * 维度为 (feature_num + 1) * target_num
  * @return 成功返回非0，失败返回0
  */
 extern int dsp_linear_regression_init(
-    size_t feature_dim,
-    size_t target_dim,
+    size_t feature_num,
+    size_t target_num,
     float32_t *xwx,
-    float32_t *ywx
+    float32_t *xwy
 );
 
-/**
- * @brief 向线性回归读入一个样本
- *
- * @param feature_dim 样本特征向量维数
- * @param target_dim 预测的目标向量维数
- * @param x 样本特征
- * @param y 样本目标
- * @param weight 样本权重，(0, 1)
- * 先前样本的权重会由于加入更多样本而衰减，所以这个权重并不等于加权线性回归的权重
- * @param xwx 先前保存的统计量矩阵，读入样本后会更新
- * @param ywx 先前保存的统计量矩阵，读入样本后会更新
- * @return 成功返回非0，失败返回0
- */
-extern int dsp_linear_regression_add_sample(
-    size_t feature_dim,
-    size_t target_dim,
+static inline void dsp_linear_regression_uni_init(
+    float32_t xwx[4],
+    float32_t xwy[2]
+)
+{
+    dsp_linear_regression_init(1, 1, xwx, xwy);
+}
+
+extern void dsp_linear_regression_uni_update(
+    float32_t x,
+    float32_t y,
+    float32_t weight,
+    float32_t xwx[4],
+    float32_t xwy[2]
+);
+
+extern int dsp_linear_regression_uni_solve(
+    const float32_t xwx[4],
+    const float32_t xwy[2],
+    float32_t l2,
+    float32_t coef[2]
+);
+
+extern float32_t dsp_linear_regression_uni_predict(
+    const float32_t *coef,
+    float32_t x
+);
+
+static inline void dsp_linear_regression_mul_init(
+    size_t feature_num,
+    float32_t *xwx,
+    float32_t *xwy
+)
+{
+    dsp_linear_regression_init(feature_num, 1, xwx, xwy);
+}
+
+extern void dsp_linear_regression_mul_update(
+    size_t feature_num,
+    const float32_t *x,
+    float32_t y,
+    float32_t weight,
+    float32_t *xwx,
+    float32_t *xwy
+);
+
+extern int dsp_linear_regression_mul_solve(
+    size_t feature_num,
+    const float32_t *xwx,
+    const float32_t *xwy,
+    float32_t l2,
+    float32_t *coef
+);
+
+extern float32_t dsp_linear_regression_mul_predict(
+    size_t feature_num,
+    const float32_t *coef,
+    const float32_t *x
+);
+
+static inline void dsp_linear_regression_mt_init(
+    size_t feature_num,
+    size_t target_num,
+    float32_t *xwx,
+    float32_t *xwy
+)
+{
+    dsp_linear_regression_init(feature_num, target_num, xwx, xwy);
+}
+
+extern void dsp_linear_regression_mt_update(
+    size_t feature_num,
+    size_t target_num,
     const float32_t *x,
     const float32_t *y,
     float32_t weight,
     float32_t *xwx,
-    float32_t *ywx
+    float32_t *xwy
 );
 
-/**
- * @brief 根据迄今为止读入的样本，求解线性回归模型
- *
- * @param feature_dim 样本特征向量维数
- * @param target_dim 预测的目标向量维数
- * @param xwx 先前保存的统计量矩阵
- * @param ywx 先前保存的统计量矩阵
- * @param l2 L2 正则化系数，[0, +inf)
- * @param param_buf 由调用者提供，供临时存储中间结果矩阵的空间，当 l2 = 0 时可以为 NULL
- * @param param_inv 保存 (X^T * W * X)^-1 的计算结果
- * @param coef 保存求解得到的模型系数
- * @return 成功返回非0，失败返回0
- */
-extern int dsp_linear_regression_solve(
-    size_t feature_dim,
-    size_t target_dim,
-    float32_t *xwx,
-    const float32_t *ywx,
+extern int dsp_linear_regression_mt_solve(
+    size_t feature_num,
+    size_t target_num,
+    const float32_t *xwx,
+    const float32_t *xwy,
     float32_t l2,
-    float32_t *param_buf,
-    float32_t *param_inv,
     float32_t *coef
 );
 
-/**
- * @brief 使用解得的线性回归模型预测目标
- *
- * @param feature_dim 样本特征向量维数
- * @param target_dim 预测的目标向量维数
- * @param coef 线性回归模型系数
- * @param x 输入特征向量
- * @param y 保存预测得到的目标向量
- * @return 成功返回非0，失败返回0
- */
-extern int dsp_linear_regression_predict(
-    size_t feature_dim,
-    size_t target_dim,
+extern void dsp_linear_regression_mt_predict(
+    size_t feature_num,
+    size_t target_num,
     const float32_t *coef,
     const float32_t *x,
     float32_t *y
 );
 
 /**
+ * @brief 向线性回归读入一个样本
+ *
+ * @param feature_num 样本特征向量维数
+ * @param target_num 预测的目标向量维数
+ * @param x 样本特征
+ * @param y 样本目标
+ * @param weight 样本权重，(0, 1)
+ * 先前样本的权重会由于加入更多样本而衰减，所以这个权重并不等于加权线性回归的权重
+ * @param xwx 先前保存的统计量矩阵，读入样本后会更新
+ * @param xwy 先前保存的统计量矩阵，读入样本后会更新
+ * @return 成功返回非0，失败返回0
+ */
+static inline int dsp_linear_regression_update(
+    size_t feature_num,
+    size_t target_num,
+    const float32_t *x,
+    const float32_t *y,
+    float32_t weight,
+    float32_t *xwx,
+    float32_t *xwy
+)
+{
+    if (1 == target_num)
+    {
+        if (1 == feature_num)
+        {
+            dsp_linear_regression_uni_update(*x, *y, weight, xwx, xwy);
+        }
+        else
+        {
+            dsp_linear_regression_mul_update(feature_num, x, *y, weight, xwx, xwy);
+        }
+    }
+    else
+    {
+        dsp_linear_regression_mt_update(
+            feature_num,
+            target_num,
+            x,
+            y,
+            weight,
+            xwx,
+            xwy
+        );
+    }
+
+    return 1;
+}
+
+/**
+ * @brief 根据迄今为止读入的样本，求解线性回归模型
+ *
+ * @param feature_num 样本特征向量维数
+ * @param target_num 预测的目标向量维数
+ * @param xwx 先前保存的统计量矩阵
+ * @param xwy 先前保存的统计量矩阵
+ * @param l2 L2 正则化系数，[0, +inf)
+ * @param coef 保存求解得到的模型系数
+ * @return 成功返回非0，失败返回0
+ */
+static inline int dsp_linear_regression_solve(
+    size_t feature_num,
+    size_t target_num,
+    const float32_t *xwx,
+    const float32_t *xwy,
+    float32_t l2,
+    float32_t *coef
+)
+{
+    if (1 == target_num)
+    {
+        if (1 == feature_num)
+        {
+            return dsp_linear_regression_uni_solve(xwx, xwy, l2, coef);
+        }
+        else
+        {
+            return dsp_linear_regression_mul_solve(feature_num, xwx, xwy, l2, coef);
+        }
+    }
+    else
+    {
+        return dsp_linear_regression_mt_solve(
+            feature_num,
+            target_num,
+            xwx,
+            xwy,
+            l2,
+            coef
+        );
+    }
+}
+
+/**
+ * @brief 使用解得的线性回归模型预测目标
+ *
+ * @param feature_num 样本特征向量维数
+ * @param target_num 预测的目标向量维数
+ * @param coef 线性回归模型系数
+ * @param x 输入特征向量
+ * @param y 保存预测得到的目标向量
+ * @return 成功返回非0，失败返回0
+ */
+static inline int dsp_linear_regression_predict(
+    size_t feature_num,
+    size_t target_num,
+    const float32_t *coef,
+    const float32_t *x,
+    float32_t *y
+)
+{
+    if (1 == target_num)
+    {
+        if (1 == feature_num)
+        {
+            *y = dsp_linear_regression_uni_predict(coef, *x);
+        }
+        else
+        {
+            *y = dsp_linear_regression_mul_predict(feature_num, coef, x);
+        }
+    }
+    else
+    {
+        dsp_linear_regression_mt_predict(feature_num, target_num, coef, x, y);
+    }
+
+    return 1;
+}
+
+/**
  * @brief 初始化一元线性回归模型
  *
+ * @param w 用来保存累加权重
  * @param ex 增量求解过程中用来保存特征的期望
  * @param ey 增量求解过程中用来保存目标的期望
  * @param ex2 增量求解过程中用来保存 x^2 的期望
@@ -112,6 +278,7 @@ extern int dsp_linear_regression_predict(
  * @note 是线性回归模型在输入和输出都是一元的情况下的简便函数
  */
 extern int dsp_linear_regression1_init(
+    float32_t *w,
     float32_t *ex,
     float32_t *ey,
     float32_t *ex2,
@@ -124,25 +291,22 @@ extern int dsp_linear_regression1_init(
  * @param x 样本特征
  * @param y 样本目标
  * @param weight 样本权重
- * @param ex 保存迄今为止 x 的期望
- * @param ey 保存迄今为止 y 的期望
- * @param ex2 保存迄今为止 x^2 的期望
- * @param exy 保存迄今为止 x * y 的期望
+ * @param xwx XWX
+ * @param xwy XWy
  * @return 成功返回非0，失败返回0
  */
 extern int dsp_linear_regression1_add_sample(
     float32_t x,
     float32_t y,
     float32_t weight,
-    float32_t *ex,
-    float32_t *ey,
-    float32_t *ex2,
-    float32_t *exy
+    float32_t xwx[2][2],
+    float32_t xwy[2]
 );
 
 /**
  * @brief 求解一元线性回归模型
  *
+ * @param w 累加权重
  * @param ex x 的期望
  * @param ey y 的期望
  * @param ex2 x^2 的期望
@@ -153,6 +317,7 @@ extern int dsp_linear_regression1_add_sample(
  * @return 成功返回非0，失败返回0
  */
 extern int dsp_linear_regression1_solve(
+    float32_t w,
     float32_t ex,
     float32_t ey,
     float32_t ex2,
